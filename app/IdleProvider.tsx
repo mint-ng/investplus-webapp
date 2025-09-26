87537932a095d427cf3cfd779186883dc591a016
Eten-Ekata <ekataelishaeten@gmail.com>
2025-09-26 08:53:33 +0100

    timer update

--- app/IdleProvider.tsx
+++ app/IdleProvider.tsx
@@ -30,8 +30,8 @@ export function IdleProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <IdleTimerProvider
-      // timeout={1000 * 60 * 1}
-      timeout={1000 * 20}
+      timeout={1000 * 60 * 1}
+      // timeout={1000 * 20}
       onIdle={handleOnIdle}
       debounce={500}
     >
